ebb0a440978be9b67ba22da9b4fc5af97d5ddaca
laurenwest12 <laurenwest12@gmail.com>
2021-03-10 11:47:13 -0500

    added sql data and initial setup for verifying if an address needs changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const USPS = require('usps-webtools');
+const sql = require('msnodesqlv8')
 
 const { username, server, database, driver } = require('./config');
 
@@ -14,34 +15,104 @@ const usps = new USPS({
     ttl: 10000 
 })
 
-const addressVerification = () => {
+/* Create a function that will check if the original address matches the returned address from USPS.
+If not, return an array of what is different. */
+
+const returnStatus = (originalAddress, updatedAddress) => {
+    let statuses = []
+
+    if (originalAddress.city.toUpperCase() !== updatedAddress.city.toUpperCase()) {
+        status.push('City')
+    }
+
+    if (originalAddress.state.toUpperCase() !== updatedAddress.state.toUpperCase()) {
+        status.push('State')
+    }
+
+    if (originalAddress.zip.toUpperCase() !== updatedAddress.zip.toUpperCase()) {
+        status.push('Zip')
+    }
+
+    return statuses
+
+}
+
+const addressVerification = (address) => {
+    const { InvoiceNumber, SoldTo, DirectToStoreAddress1, DirecttoStoreAddress2, DirecttoStoreCity, DirecttoStoreState, DirecttoStoreZip } = address
+
+    const originalAddress1 = DirectToStoreAddress1.trim()
+    const originalAddress2 = DirecttoStoreAddress2.trim()
+    const originalCity = DirecttoStoreCity.trim()
+    const originalState = DirecttoStoreState.trim()
+    const originalZip = DirecttoStoreZip.trim()
+
     usps.verify({
-        street1: '',
-        street2: 'Apt 2',
-        city: 'San Francisco',
-        state: 'CA',
-        zip: '94107'
+        street1: originalAddress1,
+        street2: '',
+        city: originalCity,
+        state: originalState,
+        zip: originalZip
     }, (err, address) => {
         if (err) {
             //If there is an error with street1, check with street2.
             usps.verify({
-                street1: '322 3rd st.',
-                street2: 'Apt 2',
-                city: 'San Francisco',
-                state: 'CA',
-                zip: '94107'
+                street1: originalAddress2,
+                street2: '',
+                city: originalCity,
+                state: originalState,
+                zip: originalZip
             }, (err, address) => {
-                console.log(address)
+                if (err) {
+                    console.log(err.message)
+                } else {
+                    const originalAddress = {
+                        address1: originalAddress1,
+                        address2: originalAddress2,
+                        city: originalCity,
+                        state: originalState,
+                        zip: originalZip
+                    }
+
+                    const updatedAddress = {
+                        address1: '',
+                        address2: address.street1,
+                        city: address.city,
+                        state: address.state,
+                        zip: address.zip
+                    }
+                }
             })
         } else {
             //Return the result from street1 if it did not error.
-            console.log(err)
+            const originalAddress = {
+                address1: originalAddress1,
+                address2: originalAddress2,
+                city: originalCity,
+                state: originalState,
+                zip: originalZip
+            }
+
+            const updatedAddress = {
+                address1: address.street1,
+                address2: '',
+                city: address.city,
+                state: address.state,
+                zip: address.zip
+            }
         }
     })
+}
+
+const selectQuery = async (query) => {
+    await sql.query(connectionString, query, (err, rows) => {
+        if (err) console.log(err)
+        //For each row in the table, verify the address.
+        rows.map(row => addressVerification(row))
+    }) 
 }
 
 
 app.listen(5000, async () => {
     console.log('App is running...')
-    const add = await addressVerification()
+    await selectQuery(selectStatement)
 })
