429d7bbb5ae14c4707f41e507b8de21c64389ec0
laurenwest12 <laurenwest12@gmail.com>
2021-03-10 10:43:10 -0500

    added in logic to check first for address1 then address2

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const USPS = require('usps-webtools');
 
-const { username } = require('./config');
+const { username, server, database, driver } = require('./config');
+
+const connectionString = `server=${server};Database=${database};Trusted_Connection=Yes;Driver=${driver}`
+
+const selectStatement = `SELECT TOP 10 * FROM GlipsumAPISummary`
 
 const usps = new USPS({
     server: 'http://production.shippingapis.com/ShippingAPI.dll',
@@ -10,17 +14,29 @@ const usps = new USPS({
     ttl: 10000 
 })
 
-
-
 const addressVerification = () => {
     usps.verify({
-        street1: '322 3rd st.',
+        street1: '',
         street2: 'Apt 2',
         city: 'San Francisco',
         state: 'CA',
         zip: '94107'
-    }, async (err, address) => {
-        console.log(address)
+    }, (err, address) => {
+        if (err) {
+            //If there is an error with street1, check with street2.
+            usps.verify({
+                street1: '322 3rd st.',
+                street2: 'Apt 2',
+                city: 'San Francisco',
+                state: 'CA',
+                zip: '94107'
+            }, (err, address) => {
+                console.log(address)
+            })
+        } else {
+            //Return the result from street1 if it did not error.
+            console.log(err)
+        }
     })
 }
 
